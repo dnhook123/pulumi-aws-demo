37428c393b7d78f231db03a7f0665ab2f9429c37
Weng,David D <d.weng@student.fontys.nl>
2022-06-25 22:06:20 +0200

    small fix

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 import * as aws from "@pulumi/aws";
 import * as pulumi from "@pulumi/pulumi"
-const stackConfig = new pulumi.Config('cr-test');
+const stackConfig = new pulumi.Config('pulumi-aws-demo');
 
 // Create an AWS resource (S3 Bucket)
-function davidwengpang() {
+function test() {
     const bucket = new aws.s3.Bucket(stackConfig.require('purposeName'));
 }
 
-module.exports = davidwengpang();
+module.exports = test();
